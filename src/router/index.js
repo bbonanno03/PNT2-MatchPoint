1d821b09a4c6d0a5d9ee650378be44377787db5b
Nicolas <nicolas.cajaraville7@gmail.com>
2026-06-09 19:50:46 -0300

    fix rutas duplicadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '../stores/auth'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
 
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import CourtsView from '../views/CourtsView.vue'
-import ReservationsView from '../views/ReservationsView.vue'
-import PoliciesView from '../views/PoliciesView.vue'
-import AdminCourtsView from '../views/AdminCourtsView.vue'
-import MetricsView from '../views/MetricsView.vue'
-import AdminUsersView from '../views/AdminUsersView.vue'
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import CourtsView from "../views/CourtsView.vue";
+import ReservationsView from "../views/ReservationsView.vue";
+import PoliciesView from "../views/PoliciesView.vue";
+import AdminCourtsView from "../views/AdminCourtsView.vue";
+import MetricsView from "../views/MetricsView.vue";
+import AdminUsersView from "../views/AdminUsersView.vue";
 
 const routes = [
-  { path: '/', redirect: '/canchas' },
-  { path: '/login', component: LoginView },
-  { path: '/register', component: RegisterView },
-  { path: '/canchas', component: CourtsView },
-  { path: '/reservas', component: ReservationsView },
-  { path: '/politicas', component: PoliciesView },
-  { path: '/admin/canchas', component: AdminCourtsView },
-  { path: '/admin/metricas', component: MetricsView },
-  { path: '/admin/usuarios', component: AdminUsersView },
-  { path: '/admin/canchas', component: AdminCourtsView, meta: { requiresAdmin: true } },
-{ path: '/admin/usuarios', component: AdminUsersView, meta: { requiresAdmin: true } },
-{ path: '/admin/metricas', component: MetricsView, meta: { requiresAdmin: true } }
-]
+  { path: "/", redirect: "/canchas" },
+  { path: "/login", component: LoginView },
+  { path: "/register", component: RegisterView },
+  { path: "/canchas", component: CourtsView },
+  { path: "/reservas", component: ReservationsView },
+  { path: "/politicas", component: PoliciesView },
+  {
+    path: "/admin/canchas",
+    component: AdminCourtsView,
+    meta: { requiresAdmin: true },
+  },
+  {
+    path: "/admin/usuarios",
+    component: AdminUsersView,
+    meta: { requiresAdmin: true },
+  },
+  {
+    path: "/admin/metricas",
+    component: MetricsView,
+    meta: { requiresAdmin: true },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to) => {
-  const authStore = useAuthStore()
+  const authStore = useAuthStore();
 
   if (to.meta.requiresAdmin && !authStore.isAdmin) {
-    return '/canchas'
+    return "/canchas";
   }
-})
+});
 
-export default router
+export default router;
