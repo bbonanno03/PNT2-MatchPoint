a94781757bfe3928dd5e5772c0b59d255b3a37c3
bbonanno03 <brunof.bonanno@gmail.com>
2026-06-09 18:08:51 -0300

    feat: agregar navbar de navegación principal

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <router-link to="/canchas">Canchas</router-link>
+    <router-link to="/reservas">Mis Reservas</router-link>
+    <router-link to="/politicas">Políticas</router-link>
+    <router-link to="/admin/canchas">Admin Canchas</router-link>
+    <router-link to="/admin/metricas">Métricas</router-link>
+    <router-link to="/login">Login</router-link>
+  </nav>
+</template>
